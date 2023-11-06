f5ee22a1d22686d8f00a0eec1f5e39936863e012
VerneZhong <verne.zhong@gmail.com>
2023-11-06 22:31:30 +0900

    创建组件和组件间相互调用

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/news/news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
+})
+export class NewsComponent {
+  public title="我是一个新闻组件";
+}
